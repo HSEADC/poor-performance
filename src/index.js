2c74d5f2e0e09acd0340d05137440ae1ee7657ea
andrrey kotov <kotovandrey2004031115@gmail.com>
2025-03-20 19:33:10 +0300

    menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./index.css";
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.location.pathname === "/ivents") {
+    document.getElementById("main-item-tab").focus();
+  }
+});
